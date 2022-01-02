c2039ba0972cc67805dc3f84e337ee6fc2f4a0a6
Rafael Caixeta <caixetadev@gmail.com>
2022-01-02 00:29:03 -0300

    mudando a cor principal

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -11,11 +11,11 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Poppins', sans-serif;
+    font-family: 'Roboto', sans-serif;
   }
   body {
     color: #fff;
-    background-color: #0b0b0b;
+    background-color: #0e141b;
   }
   ul {
     list-style: none;
