3018a61c1768c58017618c7c94d198c8278d45d9
Rafael Caixeta <caixetadev@gmail.com>
2022-01-06 21:12:35 -0300

    consumindo a api com static props

--- src/components/Sections/Projects/index.tsx
+++ src/components/Sections/Projects/index.tsx
@@ -1,22 +1,9 @@
 import { Container } from "../../../styles/global";
 import swal from "sweetalert";
-import { useEffect, useState } from "react";
 import SectionProjects from "./style";
 
-export default function Projects() {
-  const [projects, setProjects] = useState<string[]>([]);
-
-  useEffect((): void => {
-    async function getProjects(): Promise<void> {
-      const url = await fetch("https://apiportfoliocaixeta.herokuapp.com/");
-      const data: string[] = await url.json();
-
-      setProjects(data);
-    }
-
-    getProjects();
-  }, []);
-
+export default function Projects({ projects }) {
+  console.log(projects);
   function onPick() {
     swal("Thanks for your rating!", `You rated us $/3`, "success");
   }
