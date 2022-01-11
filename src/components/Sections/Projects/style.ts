808b6834c45b5480d04c8551dc1393b4550dbdaf
Rafael Caixeta <caixetadev@gmail.com>
2022-01-11 16:15:01 -0300

    começando a criar os card dos projects

--- src/components/Sections/Projects/style.ts
+++ src/components/Sections/Projects/style.ts
@@ -16,6 +16,54 @@ const SectionProjects = styled.section`
     bottom: 2px;
     z-index: -1;
   }
+
+  & .cont {
+    display: flex;
+    gap: 4rem;
+    flex-wrap: wrap;
+    margin-top: 5rem;
+  }
+`;
+
+export const Card = styled.div`
+  background-color: ${(props) => props.theme.colors.bakground_secundary};
+  height: 40rem;
+  margin-bottom: 5rem;
+  border-radius: 1rem;
+
+  & h3 {
+    font-size: 2.5rem;
+  }
+
+  & .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+  }
+
+  & .header ul {
+    display: flex;
+  }
+
+  & .header ul li .github svg {
+    width: 2.3rem;
+    height: 2.3rem;
+  }
+
+  & .header ul li .link svg {
+    width: 2rem;
+    height: 2rem;
+  }
+
+  & .header ul li + li {
+    margin-left: 2rem;
+  }
+
+  & img {
+    border-radius: 1rem 1rem 0 0;
+    object-fit: cover;
+  }
 `;
 
 export default SectionProjects;
