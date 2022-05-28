866f28aa57a093150badd4f8260b96dc71686762
Rafael Caixeta <caixetadev@gmail.com>
2022-05-28 19:37:39 -0300

    feat: initialize hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
+import { useEffect } from "react";
+
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
+
+import { hotjar } from "react-hotjar";
 
 import { Header } from "../components";
 
@@ -7,6 +11,10 @@ import GlobalStyle from "../styles/global";
 import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(2993174, 6);
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <Header />
