efa67bda2b7023c502f9059981831b1e3cb82b09
Rafael Caixeta <caixetadev@gmail.com>
2022-05-30 18:16:12 -0300

    refactor: changing section about styles

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Container } from "../../../styles/global";
-import { SectionTitle } from "../../../components";
+import { Container } from "../../styles/global";
+import { Title } from "..";
 
 import * as S from "./style";
 
@@ -9,14 +9,15 @@ export function About() {
     <S.SectionAbout id="about">
       <Container>
         <div>
-          <SectionTitle title="About" />
+          <Title>About</Title>
           <S.Paragraph data-aos="fade-up">
             Hi, my name is Rafael Caixeta, I'm a self-taught and freelance web
             developer, I'm currently delving into Frontend development, focusing
             on building responsive interfaces, writing clean and efficient code.
             Passionate about technology, books, games and music. My favorite
-            techs are: React, JavaScript, Typescript and Next.js. You can
-            contact me through{"  "}
+            techs are: <span>React</span>, <span>Typescript</span> and{" "}
+            <span>Nextjs</span>. You can contact me through
+            {"  "}
             <S.Link
               target="_blank"
               rel="noreferrer"
---
+++ src/components/About/style.ts
@@ -0,0 +1,38 @@
+import styled, { css } from "styled-components";
+
+import About from "/public/assets/about.svg";
+
+export const SectionAbout = styled.section`
+  height: 40rem;
+  max-width: 120rem;
+  margin: 15rem auto 0 auto;
+  background-image: url(${About.src});
+  background-repeat: no-repeat;
+
+  @media (max-width: 560px) {
+    height: 65rem;
+  }
+`;
+
+export const Paragraph = styled.p`
+  line-height: 3.5rem;
+  text-align: center;
+  font-weight: 300;
+  margin: 0 auto;
+  max-width: 90%;
+  font-size: 1.6rem;
+
+  span {
+    ${({ theme }) => css`
+      text-decoration: underline;
+      text-decoration-color: ${theme.colors.text_blue};
+    `}
+  }
+`;
+
+export const Link = styled.a`
+  ${({ theme }) => css`
+    text-decoration: underline;
+    text-decoration-color: ${theme.colors.text_blue};
+  `}
+`;
