1586a47cc180b07546ba51f77d4d87cbfb2026bc
Rafael Caixeta <caixetadev@gmail.com>
2022-05-30 18:03:13 -0300

    refactor: removing cdn from Roboto source and adding to Poppins cdn

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,8 +38,14 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
