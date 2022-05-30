5d80a31cf54f182ac491a6de8b491c5d5a87e034
Rafael Caixeta <caixetadev@gmail.com>
2022-05-30 18:06:56 -0300

    refactor: changing navbar styles

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+
+import { useTranslation } from "next-i18next";
+
+import { Container } from "../../styles/global";
+import Globo from "/public/assets/globo.svg";
+import Logo from "/public/assets/logo.svg";
+
+import { Link } from "react-scroll";
+
+import * as S from "./style";
+import { useRouter } from "next/router";
+
+export function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [nav, setShowNav] = useState(false);
+
+  const { t } = useTranslation("common");
+
+  const router = useRouter();
+
+  const onChangeLanguage = (lang: string) => () => {
+    router.push(router.asPath, undefined, { locale: lang });
+  };
+
+  useEffect(() => {
+    const scroll = () => {
+      if (window.scrollY > 20) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", scroll);
+
+    return () => window.removeEventListener("scroll", scroll);
+  }, []);
+
+  useEffect(() => {
+    nav
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "visible");
+  }, [nav]);
+
+  return (
+    <S.Header isScrolled={isScrolled}>
+      <Container>
+        <S.Wrapper>
+          <Link to="home" spy={false} smooth="easeInOutQuart" duration={1000}>
+            <S.Logo src={Logo.src} />
+          </Link>
+          <S.Nav>
+            <S.NavItens>
+              <S.Items>
+                <Link
+                  to="about"
+                  spy={false}
+                  offset={-250}
+                  smooth="easeInQuint"
+                  duration={700}
+                >
+                  About
+                </Link>
+              </S.Items>
+              <S.Items>
+                <Link
+                  to="projects"
+                  spy={false}
+                  offset={-150}
+                  smooth="easeInQuint"
+                  duration={700}
+                >
+                  Projects
+                </Link>
+              </S.Items>
+              <S.Items>
+                <S.Link
+                  href="https://linkedin.com/in/caixetadev"
+                  target="_blank"
+                >
+                  Linkedin
+                </S.Link>
+              </S.Items>
+              <S.Items>
+                <S.Link>Curriculum</S.Link>
+              </S.Items>
+              <S.Items>
+                <S.Link onClick={onChangeLanguage("pt-BR")}>
+                  <S.Icon src={Globo.src} />
+                </S.Link>
+              </S.Items>
+              <S.Items>
+                <Link
+                  to="contact"
+                  spy={false}
+                  smooth="easeInOutQuart"
+                  duration={1000}
+                >
+                  Contact
+                </Link>
+              </S.Items>
+            </S.NavItens>
+          </S.Nav>
+        </S.Wrapper>
+      </Container>
+    </S.Header>
+  );
+}
---
+++ src/components/Navbar/style.ts
@@ -0,0 +1,67 @@
+import styled, { css } from "styled-components";
+
+type PropsHeader = {
+  isScrolled: boolean;
+};
+
+export const Header = styled.header<PropsHeader>`
+  ${({ theme, isScrolled }) => css`
+    display: flex;
+    align-items: center;
+    background-color: ${isScrolled
+      ? theme.colors.background_transparent
+      : theme.colors.background};
+    width: 100%;
+    height: ${isScrolled ? "7rem" : "8rem"};
+    backdrop-filter: blur(5px);
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    transition: all 0.3s ease;
+  `}
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Nav = styled.nav``;
+
+export const NavItens = styled.ul`
+  display: flex;
+`;
+
+export const Items = styled.li`
+  & + li {
+    margin-left: 5rem;
+  }
+
+  ${({ theme }) => css`
+    a,
+    img {
+      transition: all 0.3s ease;
+    }
+    a:hover,
+    img:hover {
+      color: ${theme.colors.text_blue};
+      cursor: pointer;
+    }
+  `}
+`;
+
+export const Logo = styled.img`
+  cursor: pointer;
+`;
+
+export const Link = styled.a`
+  font-size: 1.4rem;
+`;
+
+export const Icon = styled.img`
+  width: 1.9rem;
+  height: 1.9rem;
+  margin-top: 0.3rem;
+`;
