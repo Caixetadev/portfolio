12d564f25799db5cddd97b49c9414ce0e1654d5a
Rafael Caixeta <caixetadev@gmail.com>
2022-05-30 18:22:07 -0300

    refactor: removing unnecessary files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,11 @@ import "aos/dist/aos.css";
 
 import {
   Navbar,
-  Skills,
   ButtonTop,
-  Projects,
   Footer,
-  HomePortfolio,
   About,
+  Hero,
+  Projects,
 } from "../components";
 
 const Home: NextPage<{ projects: Array<IProjects> }> = ({ projects }) => {
@@ -25,25 +24,35 @@ const Home: NextPage<{ projects: Array<IProjects> }> = ({ projects }) => {
   return (
     <>
       <Navbar />
-      <HomePortfolio />
+      <Hero />
       <About />
-      <Skills />
-      <Projects projects={projects} />
+      <Projects projects={[]} />
       <ButtonTop />
       <Footer />
     </>
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
-  const url = await fetch("https://apiportfoliocaixeta.herokuapp.com/");
+// export const getStaticProps: GetStaticProps = async () => {
+//   const url = await fetch("https://apiportfoliocaixeta.herokuapp.com/");
 
-  const data: IProjects = await url.json();
+//   const data: IProjects = await url.json();
 
+//   return {
+//     props: { projects: data },
+//     revalidate: 60 * 60 * 8,
+//   };
+// };
+
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export async function getStaticProps({ locale }) {
   return {
-    props: { projects: data },
-    revalidate: 60 * 60 * 8,
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "header"])),
+      // Will be passed to the page component as props
+    },
   };
-};
+}
 
 export default Home;
