6ea428eebd035b10c1808f3169b380c301b2e45f
Rafael Caixeta <caixetadev@gmail.com>
2022-05-31 16:30:30 -0300

    chore: changing page file extensions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const { i18n } = require("./next-i18next.config");
 /** @type {import('next').NextConfig} */
 module.exports = {
+  pageExtensions: ["tsx"],
   reactStrictMode: true,
   i18n,
   images: {
