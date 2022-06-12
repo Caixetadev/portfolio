9f1073992be0cfe9771ce4d9b177f1c7bb29aac5
caixeta <caixetadev@gmail.com>
2022-06-12 00:00:16 -0300

    feat: creating Dropdown component

--- src/components/GlobeDropdown/index.tsx
+++ src/components/GlobeDropdown/index.tsx
@@ -5,9 +5,9 @@ import { Dropdown } from "../Dropdown";
 import { options } from "./options";
 import * as S from "./style";
 
-import Globo from "/public/assets/globo.svg";
+import Globe from "/public/assets/globe.svg";
 
-export function GloboDropdown() {
+export function GlobeDropdown() {
   const router = useRouter();
   const onChangeLanguage = (lang: string) => () => {
     router.push(router.asPath, undefined, { locale: lang });
@@ -15,7 +15,7 @@ export function GloboDropdown() {
 
   return (
     <Dropdown
-      title={<Image src={Globo.src} width={20} height={20} alt="Globo" />}
+      title={<Image src={Globe.src} width={20} height={20} alt="Globo" />}
     >
       <S.Content>
         {options.map(({ flag, name, value }) => (
