78da76402150ef10be7645b1453cba109ff643eb
Rafael Caixeta <caixetadev@gmail.com>
2022-12-27 06:20:11 -0300

    chore: add setup jest

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jest-environment-jsdom",
 };
 
