8d6a67ba549308f25e1b620adf46d5ed5f6b1bf0
Rafael Caixeta <caixetadev@gmail.com>
2023-01-07 09:43:47 -0300

    chore: generating an exception if the context does not exist

--- src/hooks/useProjectsContext.ts
+++ src/hooks/useProjectsContext.ts
@@ -4,6 +4,10 @@ import { useContext } from "react";
 export function useProjectsContext() {
   const { projects } = useContext(ProjectsContext);
 
+  if (!projects) {
+    throw new Error("Context used outside of its Provider!");
+  }
+
   return {
     projects,
   };
