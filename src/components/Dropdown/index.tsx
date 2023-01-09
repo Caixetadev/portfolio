1e6aa39d4b0d3cca35c0354cd7669978e3d4d8f0
Rafael Caixeta <caixetadev@gmail.com>
2023-01-09 07:25:28 -0300

    feat: closing dropdown when clicking anywhere on the screen and when selecting a language

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 
 import { useToggle } from "hooks/useToggle";
 
@@ -12,10 +12,19 @@ import * as S from "./style";
 export function Dropdown({ title, children }: DropdownProps) {
   const { state, toggle } = useToggle();
 
+  useEffect(() => {
+    state
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "visible");
+  }, [state]);
+
   return (
-    <S.Wrapper>
-      <S.Title onClick={toggle}>{title}</S.Title>
-      <S.Content isOpen={state}>{children}</S.Content>
-    </S.Wrapper>
+    <>
+      <S.Wrapper isOpen={state}>
+        <S.Title onClick={toggle}>{title}</S.Title>
+        <S.Content onClick={toggle}>{children}</S.Content>
+        <S.Overlay onClick={toggle} />
+      </S.Wrapper>
+    </>
   );
 }
--- src/components/Dropdown/style.ts
+++ src/components/Dropdown/style.ts
@@ -1,33 +1,67 @@
 import styled, { css } from "styled-components";
 
-type ContentProps = {
+type WrapperProps = {
   isOpen: boolean;
 };
 
-export const Wrapper = styled.div`
-  position: relative;
-  width: max-content;
-`;
+const wrapperModifiers = {
+  open: () => css`
+    opacity: 1;
+    pointer-events: auto;
+    transform: translateY(0);
+  `,
 
-export const Content = styled.div<ContentProps>`
+  close: () => css`
+    opacity: 0;
+    pointer-events: none;
+    transform: translateY(-2rem);
+  `,
+};
+
+export const Wrapper = styled.div<WrapperProps>`
   ${({ isOpen }) => css`
-    transition: all 0.3s ease;
-    display: ${isOpen ? "block" : "none"};
-    opacity: ${isOpen ? "1" : "0"};
-    position: absolute;
-    margin-top: 2rem;
-    right: -60px;
+    position: relative;
+    width: max-content;
 
-    &::before {
-      content: "";
-      position: absolute;
-      border-right: 1.2rem solid transparent;
-      border-left: 1.2rem solid transparent;
-      border-bottom: 1.2rem solid #111;
-      top: -1.2rem;
-      right: 58px;
+    ${Content}, ${Overlay} {
+      ${isOpen && wrapperModifiers.open()}
+      ${!isOpen && wrapperModifiers.close()}
     }
   `}
 `;
 
-export const Title = styled.div``;
+export const Overlay = styled.div`
+  ${({ theme }) => css`
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100vh;
+    cursor: initial;
+  `}
+`;
+
+export const Content = styled.div`
+  transition: all 0.3s ease;
+  position: absolute;
+  margin-top: 2rem;
+  right: -60px;
+  z-index: 100;
+
+  &::before {
+    content: "";
+    position: absolute;
+    border-right: 1.2rem solid transparent;
+    border-left: 1.2rem solid transparent;
+    border-bottom: 1.2rem solid #111;
+    top: -1.2rem;
+    right: 58px;
+  }
+`;
+
+export const Title = styled.div`
+  z-index: 50;
+  position: relative;
+`;
