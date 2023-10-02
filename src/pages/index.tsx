9f23a9f40cdd1d3f9e826399da0de735d5a9111a
Rafael Caixeta <caixetadev@gmail.com>
2023-10-02 23:05:39 +0200

    feat: add cutez project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,23 +29,30 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
 
   const mock = [
     {
-      __typename: "Project",
+      link: "https://cutez.vercel.app/",
+      image:
+        "https://cdn.discordapp.com/attachments/643825264513646593/1158505243601031369/870shots_so.png?ex=651c7d96&is=651b2c16&hm=78784b0d455e0ad32a326c5d39db4ed513f3de5b67d52d1e9ffc5ef0b88487d9&",
+      title: "Cutez",
+      description:
+        "Gerar links encurtados facilmente e gerenciá-los de forma eficiente em um painel centralizado único.",
+      createdAt: "2023-10-02T20:17:31.357618+00:00",
+    },
+    {
       link: "https://trampodecasa.com.br/",
       image:
-        "https://cdn.discordapp.com/attachments/963219945204887642/991730441595924510/unknown.png",
+        "https://cdn.discordapp.com/attachments/889302050238775297/1158498981723259020/429shots_so.png?ex=651c77c1&is=651b2641&hm=8cf5a1fe7acb2b9617c35fed39cd89a1153412cede83c4cb9a400057ebfff33a&",
       title: "Trampo de Casa",
       description:
-        "Trabalho freelance feito com o Next Js, onde você pode se candidatar a vagas remotas",
+        "Trabalho freelance realizado com Next.js, onde você pode se candidatar a posições de trabalho remoto.",
       createdAt: "2022-12-15T21:22:14.395647+00:00",
     },
     {
-      __typename: "Project",
       link: "https://caixetadev.github.io/itachi/",
       image:
-        "https://camo.githubusercontent.com/73a022e7efa4a33825349d6e8a5ea2bc4cfc3442ef4c69d55656ccb9136d4295/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3837313134313233343236343134353933322f3931303031313633363630343130303630382f756e6b6e6f776e2e706e673f77696474683d383834266865696768743d343133",
+        "https://cdn.discordapp.com/attachments/889302050238775297/1158500717443690658/65shots_so.png?ex=651c795e&is=651b27de&hm=ae749f07f4a0eb2d2c636db59bae6e7700439504cc58a4779fc72e21796880a4&",
       title: "Itachi Uchiha",
       description:
-        "Este projeto eu fiz quando estava começando a aprender SASS, projeto baseado em um design de drible",
+        "Este projeto eu fiz quando estava começando a aprender SASS, com base em um design do Dribbble.",
       createdAt: "2022-06-27T19:18:47.359937+00:00",
     },
   ];
